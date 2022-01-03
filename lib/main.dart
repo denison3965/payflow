bd2e25cf74416c2bccd1c2c2a0454aad100262d4
Denison <denisondealmeidaprtela@hotmail.com>
2022-01-03 20:57:38 -0300

    Setting the firebase on aplication (Android (java), Flutter)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/modulos/home/home_page.dart';
 import 'package:payflow/modulos/login/login_page..dart';
@@ -5,22 +6,38 @@ import 'package:payflow/modulos/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
-  runApp(const AppWidget());
+  runApp(const AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({Key? key}) : super(key: key);
 
+  @override
+  State<AppFirebase> createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary
-      ),
-      home: LoginPage(),
-    );
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyAwesomeApp();
+        }
+
+        return Loading();
+      },
+    )
   }
 }
+
+
 
